89421eece9663043dcb01c4019ff8b800109c1a8
Bruno Duarte <brunoduarte1977@gmail.com>
2024-10-08 13:36:35 -0300

    Configurando Swagger para a passagem do tokem JWT

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ using System.Text;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.OpenApi.Models;
+using System.Security.Cryptography.Xml;
 
 #region Builder
 var builder = WebApplication.CreateBuilder(args);
@@ -26,7 +28,9 @@ builder.Services.AddAuthentication(option => {
     }).AddJwtBearer(options => {
         options.TokenValidationParameters = new TokenValidationParameters{
             ValidateLifetime = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+            ValidateIssuer = false,
+            ValidateAudience = false
         };
 });
 
@@ -36,7 +40,31 @@ builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme{
+        Name = "Administration",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Insira o token JWT"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme{
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
 
 builder.Services.AddDbContext<DbContexto>(options =>
 {
@@ -50,7 +78,7 @@ var app = builder.Build();
 #endregion
 
 #region Home
-app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
+app.MapGet("/", () => Results.Json(new Home())).AllowAnonymous().WithTags("Home");
 #endregion
 
 #region Administradores
@@ -92,7 +120,7 @@ app.MapPost("administradores/login", ([FromBody] LoginDTO loginDTO, IAdministrad
     }
     else
         return Results.Unauthorized();
-}).WithTags("Administradores");
+}).AllowAnonymous().WithTags("Administradores");
 
 //############################################################################
 ErrosDeValidacao validaAdnDTO(AdministradorDTO administradorDTO)
