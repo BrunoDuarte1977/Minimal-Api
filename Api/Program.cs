6df42a4f5a81d6f4888f73cdb3c03d359fc14a78
Bruno Duarte <brunoduarte1977@gmail.com>
2024-10-18 16:40:16 -0300

    Add teste conexao

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,9 +1,26 @@
 using Minimal_API;
+using MySql.Data.MySqlClient;
+using System;
+
+        string connectionString = "Server=localhost;User Id=root;Password=admin;Database=minimal_api";
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexão bem-sucedida!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
 
 IHostBuilder CreateHostBuilder(string[] args){
     return Host.CreateDefaultBuilder(args)
     .ConfigureWebHostDefaults(webBuilder =>{
         webBuilder.UseStartup<Startup>();
+        //webBuilder.UseStartup<TesteConexao>();
     });
 };
 
